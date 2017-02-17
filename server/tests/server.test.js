b12f0109b51e743c593ffc2cf1bd5d1b4ce37676
baconYao <bacon735392@gmail.com>
2017-02-17 22:34:03 +0800

    Add teste for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,21 @@
 const expect = require('expect');
 const request = require('supertest');
 
-
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+//資料集，會在17行被加入DB 
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
+
 //每次測試時，都會將db清空
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -27,7 +35,7 @@ describe('POST /todos', () => {
         }
 
         // find data from db
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);         //db內，todos collection內的資料量
           expect(todos[0].text).toBe(text);
           done();
@@ -47,9 +55,31 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);       //找不到資料
+          expect(todos.length).toBe(2);       //除了7~12行新增的兩筆外，找不到其他的資料
           done();
         }).catch((err) => done(err));
       });
   });
 });
+
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);     //會取得6~12行存取的那兩筆data
+      })
+      .end(done);
+  });
+});
+
+
+
+
+
+
+
+
+
